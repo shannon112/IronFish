32d26bb9ac71976bc1b8c4c88d552e6f4042c9ef
shannon112 <b03505011@ntu.edu.tw>
2019-01-04 13:52:25 +0800

    cropped map

---
+++ simple_node/src/simple_publisher.cpp
@@ -0,0 +1,22 @@
+#include "ros/ros.h"
+#include "std_srvs/Empty.h"
+#include <cstdlib>
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "publisher_node");
+  ros::NodeHandle n;
+  ros::ServiceClient clear_map_client;
+  clear_map_client=n.serviceClient<std_srvs::Empty>("move_base/clear_costmaps");
+  std_srvs::Empty srv;
+  if (clear_map_client.call(srv))
+  {
+    ROS_INFO("Success!");
+  }
+  else
+  {
+    ROS_ERROR("Failed to call service");
+    return 1;
+  }
+  return 0;
+}
